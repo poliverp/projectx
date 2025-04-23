2e5a03f526c18a24690917ee28b59d1e7e9b0fd7
Oliver Pizarro <oliver@aa.law>
2025-04-23 11:53:32 -0700

    work

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,10 +1,10 @@
 # --- backend/extensions.py ---
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import CORS
 from flask_cors import CORS
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
 cors = CORS() # Initialize CORS here too
+login_manager = LoginManager()
