99b8b355346b9986391eb949578ca52509e731ed
Oliver Pizarro <oliver@aa.law>
2025-04-25 09:28:37 -0700

    format

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'; // Import the provider
 import 'antd/dist/reset.css'; // <-- ADD THIS IMPORT FOR ANT DESIGN STYLES
-import './index.css' // Your existing global style
 import { BrowserRouter } from 'react-router-dom'; // <-- Import BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
